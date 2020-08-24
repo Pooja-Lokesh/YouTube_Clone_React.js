474e47fc213db28c5e7ad572048111f133ddc262
Pooja Lokesh <poojadeelok@gmail.com>
2020-08-24 20:15:35 +0530

    youtube_clone

--- src/App.js
+++ src/App.js
@@ -5,23 +5,45 @@ import { Grid } from '@material-ui/core'; // curley braces used because we need 
 import youtube from "./api/youtube";
 import SearchBar from "./components/SearchBar";
 import VideoDetail from "./components/VideoDetail";
-import VideoList from "./components/VideoList";
+// import VideoList from "./components/VideoList";
 
 
 class App extends React.Component {
+
+    state = {
+        video: [],
+        selectedVideos: null,
+    }
+
+    handleSubmit = async(searchTerm) => {
+        const response = await youtube.get('search', {
+            params: {
+                part: 'snippet', // fetches videos
+                maxResults: 5, // fetches only 5 videos
+                key: 'AIzaSyBeqEYKQMDMhfPeCmpIL7Fvkcl3rfmcyVY',
+                q: searchTerm,
+            }
+        });
+
+        this.setState({ videos: response.data.items, selectedVideos: response.data.items[0] });
+    }
+
     render() {
+        const { selectedVideo } = this.state;
         return ( <
             Grid justify = 'center'
-            container spacing = { 16 } >
+            container spacing = { 10 } >
             <
             Grid item xs = { 11 } >
             <
-            Grid container spacing = { 16 } >
+            Grid container spacing = { 10 } >
             <
-            Grid item xs = { 12 } > < SearchBar / > <
-            /Grid> <
-            Grid item xs = { 8 } > < VideoDetail / > <
-            /Grid> <
+            Grid item xs = { 12 } > < SearchBar onFormSubmit = { this.handleSubmit }
+            / > < /
+            Grid > <
+            Grid item xs = { 8 } > < VideoDetail video = { this.state.selectedVideo }
+            /> < /
+            Grid > <
             Grid item xs = { 4 } > { /* Search Bar */ } <
             /Grid> < /
             Grid > <
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
+import { Paper, TextField } from '@material-ui/core'
+
 class SearchBar extends React.Component {
     state = {
         searchTerm: '',
     }
 
+    handleChange = (event) => {
+        this.setState({ searchTerm: event.target.value });
+    }
+
+    handleSubmit = () => {
+        const { searchTerm } = this.state;
+        const { onFormSubmit } = this.props;
+
+        onFormSubmit(searchTerm);
+    }
+
     render() {
         return ( <
-            h1 > This is Search bar < /h1>
+            Paper elevation = { 6 }
+            style = {
+                { padding: '25px' }
+            } >
+            <
+            form onSubmit = { this.handleSubmit } >
+            <
+            TextField fullWidth label = 'Search...'
+            onChange = { this.handleChange } > < /TextField> < /
+            form > <
+            /Paper>
         )
     }
 }
